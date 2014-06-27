92ac6f4fe5b90e82e42e7aa2ee8a2896d1a9df01
tianlinliu <tianlin@tianlins-mbp.poly.edu>
2014-06-27 15:15:12 -0400

    Initialize chapter 1 solutions.

---
+++ chapter1/Solution1.java
@@ -0,0 +1,17 @@
+/**
+ * Problem:
+ * Determine whether a string is unique.
+ */
+
+public class Solution1 {
+	/**
+	 * Solution:
+	 *
+	 * Analysis:
+	 * Time Complexity:
+	 * Space Complexity:
+	 */
+	public static boolean isUnique(String str) {
+
+    }
+}
