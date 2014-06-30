cb33b6bc1933f4a9edb97613322e43459acb2790
Rio Shen <rioxshen@gmail.com>
2014-06-30 15:04:27 -0400

    Update Solution1.java

--- chapter1/Solution1.java
+++ chapter1/Solution1.java
@@ -1,35 +1,35 @@
 /**
- * Problem:
- * Determine whether a string is unique.
- */
+* Problem:
+* Determine whether a string is unique.
+*/
 
 public class Solution1 {
-	/**
-	 * Solution:
-	 *
-	 * Analysis:
-	 * Time Complexity:
-	 * Space Complexity:
-	 */
-	public static boolean isUnique(String str) {
-		
+    /**
+     * Solution:
+     *
+     * Analysis:
+     * Time Complexity:
+     * Space Complexity:
+     */
+    public static boolean isUnique(String str) {
+
     }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         // unique string returns true
-		assert (isUniqueArray("abcdef") == true);
-		assert (isUniqueBit("abcdef") == true);
-		assert (isUniqueHash("abcdef") == true);
-		
-		// empty string is unique.
-		assert (isUniqueArray(null) == true);
-		assert (isUniqueBit(null) == true);
+        assert (isUniqueArray("abcdef") == true);
+        assert (isUniqueBit("abcdef") == true);
+        assert (isUniqueHash("abcdef") == true);
+
+        // empty string is unique.
+        assert (isUniqueArray(null) == true);
+        assert (isUniqueBit(null) == true);
         assert (isUniqueHash(null) == true);
-		 
-		// normal test
+
+        // normal test
         assert (isUniqueArray("aaaaa") == false);
         assert (isUniqueBit("aaaaa") == false);
         assert (isUniqueHash("aaaaa") == false);
 
-	}
+    }
 }
