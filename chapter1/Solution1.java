da9c4a6c6f1e50ee8445d5db4fd12db2adb80d23
Rio Shen <rioxshen@gmail.com>
2014-06-30 15:01:46 -0400

    add test cases in main function

--- chapter1/Solution1.java
+++ chapter1/Solution1.java
@@ -12,6 +12,24 @@ public class Solution1 {
 	 * Space Complexity:
 	 */
 	public static boolean isUnique(String str) {
-
+		
     }
+
+	public static void main(String[] args) {
+        // unique string returns true
+		assert (isUniqueArray("abcdef") == true);
+		assert (isUniqueBit("abcdef") == true);
+		assert (isUniqueHash("abcdef") == true);
+		
+		// empty string is unique.
+		assert (isUniqueArray(null) == true);
+		assert (isUniqueBit(null) == true);
+        assert (isUniqueHash(null) == true);
+		
+		// normal test
+        assert (isUniqueArray("aaaaa") == false);
+        assert (isUniqueBit("aaaaa") == false);
+        assert (isUniqueHash("aaaaa") == false);
+
+	}
 }
