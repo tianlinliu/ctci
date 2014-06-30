9f3ffcd6939279838b40d99e743244540e27d8ad
tianlinliu <tianlin@172-29-35-61.dynapool.nyu.edu>
2014-06-30 15:33:15 -0400

    update

--- chapter2-Linked Lists/Solution2.java
+++ chapter2-Linked Lists/Solution2.java
@@ -1,3 +1,15 @@
+/**
+ * Question 2.2:
+ * Implement an algorithm to find the kth to last element of a singly linked list.
+ */
+
 public class Solution2 {
 
+    public static Node findKthToLast(Node head, int k) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
