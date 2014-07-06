001b0d0ca4aae5aa3be0e09821de420284540283
tianlinliu <tianlin@Tianlins-MacBook-Pro.local>
2014-07-06 00:26:04 -0400

    update

---
+++ chapter3-Stacks-and-Queues/Solution1.java
@@ -0,0 +1,15 @@
+/**
+ * Question 3.1:
+ * Use a single array to implement three stacks.
+ */
+
+public class Solution {
+    /**
+     *
+     */
+    public static
+
+    public static void main(String[] args) {
+
+    }
+}
