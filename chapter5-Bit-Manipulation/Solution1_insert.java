6e83e638582bc8af25e963f6aa303cc43c911f36
tianlinliu <tianlin@Tianlins-MacBook-Pro.local>
2014-07-24 17:31:56 -0400

    update

---
+++ chapter5-Bit-Manipulation/Solution1_insert.java
@@ -0,0 +1,12 @@
+/**
+ * Question 5.1:
+ * Given two 32-bit numbers, N and M, and two bit positons, i and j. Write a method to
+ * insert M into N such that M starts at bit j and ends at bit i. Assume that the bits j
+ * through i have enough space to fit all of M.
+ */
+
+public class Solution1_insert {
+    public static int insert(int n, int m, int i, int j) {
+
+    }
+}
