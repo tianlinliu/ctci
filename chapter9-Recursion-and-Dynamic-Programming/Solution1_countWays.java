8f22cd1a27ba655d01a3e522cad65ff8ecbec094
tianlinliu <tianlin@172-29-1-219.dynapool.nyu.edu>
2014-08-08 11:48:55 -0400

    update

---
+++ chapter9-Recursion-and-Dynamic-Programming/Solution1_countWays.java
@@ -0,0 +1,19 @@
+/**
+ * A child is running up a staircase with n steps, and can hop either 1 step, 2 steps,
+ * or 3 steps at a time. Implement a method to count how many possible ways the child can
+ * run up the stairs.
+ */
+
+public class Solution1_countWays {
+    public static int count_recursive(int n) {
+        if (n < 0) return 0;
+        if (n = 0) return 1;
+        return  countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+    }
+
+    public static int count_DP(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+
+    }
+}
---
+++ chapter9-Recursion-and-Dynamic-Programming/Solution2_countPaths.java
@@ -0,0 +1,14 @@
+/**
+ * A robot sitting on the upper left corner of an X by Y grid. The robot can only move in
+ * two directions: right and down. How many possible paths are there for the robot to go
+ * from (0,0) to (X,Y)?
+ * FOLLOW UP:
+ * Certain spots are "off limits", such that robo cannot step on them. Design
+ * an algorithm to find a path for the robot from the top left to buttom right.
+ */
+
+public class Solution2_countPaths {
+    public static int countPaths(int X, int Y) {
+
+    }
+}
